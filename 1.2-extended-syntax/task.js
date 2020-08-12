2296981dc0a996e88fa48b1f4ba4497cb5955b67
yarno <yarno@me.com>
2020-08-12 09:26:40 +0200

    Added task solution 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,52 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+
+let d;
+let x = [];
+
+    
+d = b**2 - 4 * a * c;
+
+
+if (d > 0) {
+	x[0] = (-b + Math.sqrt(d)) / 2 * a ;
+	x[1] = (-b - Math.sqrt(d)) / 2 * a ;
+} else if (d == 0) { 
+	x[0] = (-b + Math.sqrt(d)) / 2 * a ;
+} else { x = []; }
+
+return x;
+
+} 
+
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+
+let marks5;
+
+if (marks.length == 0) {
+ marks5 = [0];
+} else if (marks.length <= 5) { marks5 = marks;
+} else if (marks.length > 5) {
+  marks5 = marks.slice(0,5);
+} 
+
+let averageMark = marks5.reduce((a, b) => (a + b)) / marks5.length;
+    
+
+     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    
+let date = new Date();
+let year = date.getFullYear();
+let yearsOfBirthday = dateOfBirthday.getFullYear();
+
+if  ( year - yearsOfBirthday > 18) {
+  result = `Не желаете ли олд-фэшн, ${name}?`;
+} else { 
+  result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+}
+    
+     return result;
 }
