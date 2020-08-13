efb2c6de5af59ac9013f5bdf6c94e3d87aaed969
yarno <yarno@me.com>
2020-08-13 18:12:07 +0200

    Added task solution 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+"use strict";
+
+   let dateNow = new Date();
+   let month = date.getMonth() - dateNow.getMonth() + (12 * (date.getFullYear() - dateNow.getFullYear()));
+
+   if (isNaN(percent + contribution + amount + month) == true) {
+     console.error('Ошибка ввода данных - введите числовые значения');
+     return;
+   } 
+   let p = percent /100 /12;
+   let payment = (amount - contribution) * (p + p/(((1 + p)** month) - 1)); 
+   let totalAmount = (payment * month).toFixed(2);
+   return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+if (name == null || name == "" || name == undefined) {
+  name = "Аноним";
+} 
+
+let str = `Привет, мир! Меня зовут ${name}`;
+
+return str;
+
 }
