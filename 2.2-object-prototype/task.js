0f9f8ec71017d1573afe9128fa8c82eae9697882
yarno <yarno@me.com>
2020-08-15 17:55:46 +0200

    Added task solution 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,31 @@
-//String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome = function () {
+  let str = this;
+  let string = str.toLowerCase(); 
+  return string === string.split('').reverse().join('');
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+    {
+    let averageMark = marks.reduce((a, b) => (a + b)) / marks.length;;
+    return Math.round(averageMark);
+
+   }
+   
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+  const date = Number(new Date(birthday));
+  const now = Number(new Date());
+  const diff = now - date;
+  const msPerDay = 24 * 60 * 60 * 1000;
+  const msYearF = (msPerDay  * 365.242 );
+  const age = Math.floor((diff / msYearF));
+    
+    if (age >= 18) {
+    return verdict = "Да";
+}
 }
