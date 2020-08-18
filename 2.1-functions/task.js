1755326e5381cfbd5828f8dd89a2e00257bbe776
yarno <yarno@me.com>
2020-08-18 19:00:41 +0200

    Fix task solution 2.1

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -5,11 +5,11 @@ let d = b**2 - 4 * a * c;
 if (d < 0) {
 	return { D: d, roots: [] }
 } else if (d == 0) {
-   let x1 =  (-b + Math.sqrt(d)) / 2 * a ;
+   let x1 =  (-b + Math.sqrt(d)) / (2 * a) ;
     return { D: d, roots: [x1] }
 } else {
-	let x1 =  (-b + Math.sqrt(d)) / 2 * a ;
-	let x2 =  (-b - Math.sqrt(d)) / 2 * a ;
+	let x1 =  (-b + Math.sqrt(d)) / (2 * a) ;
+	let x2 =  (-b - Math.sqrt(d)) / (2 * a) ;
 	return { D: d, roots: [x1, x2] }
 }
 }
@@ -27,52 +27,54 @@ function showSolutionsMessage( a, b, c ) {
   }
 }
 
-
 function getAverageScore(data) {
-  
-   function getAverageMark(marks){
-     return averageMark = marks.reduce((a, b) => (a + b)) / marks.length;;
-   }
-   
+  if (data === undefined) {
+    return 0;
+  } else {
+  console.log(data);
+   let avMark = {};
    let temp = [];
-
    for (let prop in data) {
-   let value = getAverageMark (data [ prop ]);
+   let value = getAverageMark (data [prop]);
    temp.push(value);
-   console.log(`${prop}: ${value}`);
-  
+  avMark[prop] = value;
 }
-console.log(`average : ${getAverageMark(temp)}`);
-
+  avMark.average = getAverageMark(temp) ;
+  return avMark;
+  }
+}
+ 
+function getAverageMark(marks){
+     if (marks.length == 0) {
+       return 0
+     } else {
+     return  marks.reduce((a, b) => (a + b)) / marks.length;
+     }
  }
 
 
-// console.log(getAverageScore ( {
-//   algebra: [2,4,5,2,3,4],
-//   geometry: [2,3,4],
-//   russian: [3, 3, 4, 5],
-//   frances: [2,3,4], 
-// }));
-
 function getPersonData(secretData) {
 
-  let data = new Object();
-
-    data.firstName = secretData.aaa,  
-    data.lastName = secretData.bbb; 
-     
+  let data = {};
+  data.firstName = secretData.aaa,  
+  data.lastName = secretData.bbb; 
+   console.log(data);
+   let decode = {}
    for (let prop in data) {
    let secret = (data [ prop ]);
-   console.log(`${prop}: ${getDecodedValue(secret)}`);
+   decode[prop] = getDecodedValue(secret);
    }
+   return decode;
+}
 
 function getDecodedValue(secret) {
+  console.log(secret);
+
   if (secret == 0 ) {
-    return "Родриго"
+    return "Родриго";
   } else {
-    return 'Эмилио'
+    return  "Эмильо";
   }
-}
 }
 
 //console.log(getPersonData({
---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,6 @@
+function parceCount(num) {
+  let string = Number.parseInt(num);
+  if (string === NaN) 
+    throw "Невалидное значение"
+  return (Number.parseInt(num));
+}
